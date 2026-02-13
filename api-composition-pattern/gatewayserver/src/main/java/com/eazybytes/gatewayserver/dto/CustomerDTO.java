d473a29d9f46eebb0ca241ae19f0068b6b859a39
Mark <mark.hungtruong@ant-intl.com>
2026-02-13 16:38:46 +0700

    first commit

---
+++ api-composition-pattern/gatewayserver/src/main/java/com/eazybytes/gatewayserver/dto/CustomerDTO.java
@@ -0,0 +1,12 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.eazybytes.gatewayserver.dto;
+
+/**
+ * @author mark.hungtruong
+ * @version CustomerDTO.java, v 0.1 2026年02月13日 4:25 PM mark.hungtruong
+ */
+public class CustomerDTO {
+}
